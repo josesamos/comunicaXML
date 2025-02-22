ee222cd746bb63220ea2307bfa3dd928cbdfeaaa
Jose Samos <jsamos@ugr.es>
2025-02-22 12:39:30 +0100

    Validar establecimiento

--- R/establecimiento.R
+++ R/establecimiento.R
@@ -1,6 +1,28 @@
-
+#' Validar datos del establecimiento
+#'
+#' Comprueba si los datos del establecimiento proporcionados están estructurados correctamente y son coherentes.
+#' Comprueba que:
+#': la clave externa de establecimiento existe en la tabla de comunicación.
+#': los campos obligatorios se completan según el país.
+#': la información del municipio está especificada correctamente según el país.
+#': se proporciona un código de establecimiento válido o detalles completos del establecimiento, pero no ambos.
+#'
+#' @param comunicacion Un data frame que representa la tabla de comunicación.
+#' Debe contener una columna de clave principal.
+#' @param establecimiento Un data frame que representa la tabla de establecimiento.
+#' Debe contener los campos necesarios:
+#' - `codigo`: Código del establecimiento.
+#' - `pais`: Código de país (ISO 3).
+#' - `codigoMunicipio`: Código de municipio (para España).
+#' - `nombreMunicipio`: Nombre del municipio (para ubicaciones no españolas).
+#'
+#' @return `TRUE`. Genera advertencias si:
+#': los datos del establecimiento están incompletos o son inconsistentes.
+#': los detalles del municipio no coinciden con los requisitos del país.
+#': se proporcionan simultáneamente un código de establecimiento y detalles adicionales.
+#'
+#' @keywords internal
 validar_establecimiento <- function(comunicacion, establecimiento) {
-
   validate_fk(comunicacion, "comunicacion", establecimiento, "establecimiento")
 
   df <- establecimiento
@@ -9,29 +31,34 @@ validar_establecimiento <- function(comunicacion, establecimiento) {
     resto_instanciado  <- sum(!is.na(df[i, ]))
     if (is.na(df$codigo[i])) {
       if (resto_instanciado < 7) {
-        stop("Faltan datos del establecimiento en la fila ", i, ".")
-      }
-      validar_pais(df$pais[i], sprintf('establecimiento (fila %d)', i))
-      if (df$pais[i] == 'ESP') {
-        validar_municipio(df$codigoMunicipio[i],
-                          sprintf('establecimiento (fila %d)', i))
-        if (!is.na(df$nombreMunicipio[i])) {
-          stop(
-            sprintf('establecimiento (fila %d)', i),
-            " -> Si el país es España (ESP), el código de municipio ha de ir informado, no así el nombre del municipio."
-          )
-        }
+        warning("Faltan datos del establecimiento en la fila ", i, ".")
       } else {
-        if ((!is.na(df$codigoMunicipio[i])) || is.na(df$nombreMunicipio[i])) {
-          stop(
-            sprintf('establecimiento (fila %d)', i),
-            " -> Si el país es distinto de España (ESP), ha de informar el nombre del municipio, no así el código de municipio."
-          )
+        validar_pais(df$pais[i], sprintf('establecimiento (fila %d)', i))
+        if (df$pais[i] == 'ESP') {
+          validar_municipio(df$codigoMunicipio[i],
+                            sprintf('establecimiento (fila %d)', i))
+          if (!is.na(df$nombreMunicipio[i])) {
+            warning(
+              sprintf('establecimiento (fila %d)', i),
+              " -> Si el país es España (ESP), el código de municipio ha de ir informado, no así el nombre del municipio."
+            )
+          }
+        } else {
+          if ((!is.na(df$codigoMunicipio[i])) ||
+              is.na(df$nombreMunicipio[i])) {
+            warning(
+              sprintf('establecimiento (fila %d)', i),
+              " -> Si el país es distinto de España (ESP), ha de informar el nombre del municipio, no así el código de municipio."
+            )
+          }
         }
       }
     } else {
       if (resto_instanciado > 2) {
-        stop("Ha de indicar los datos del establecimiento o bien el código de establecimiento.")
+        warning(
+          sprintf('establecimiento (fila %d)', i),
+          " -> Ha de indicar los datos del establecimiento o bien el código de establecimiento."
+        )
       }
     }
   }
--- tests/testthat/test-establecimiento.R
+++ tests/testthat/test-establecimiento.R
@@ -1,3 +1,66 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("validar_establecimiento detects missing foreign key", {
+  comunicacion <- data.frame(comunicacion_pk = 1:3)
+  establecimiento <- data.frame(codigo = c(1, 2, 3), pais = c("ESP", "FRA", "ESP"))
+
+  expect_warning(validar_establecimiento(comunicacion, establecimiento),
+                 "La columna 'comunicacion_fk' no existe en la hoja 'establecimiento'.")
+})
+
+test_that("validar_establecimiento detects missing establishment data (less than 7 fields)", {
+  comunicacion <- data.frame(comunicacion_pk = 1:3)
+  establecimiento <- data.frame(comunicacion_fk = 1, codigo = c(NA),
+                                pais = c(NA), codigoMunicipio = c(NA),
+                                nombreMunicipio = c(NA), extra1 = NA, extra2 = NA)
+
+  expect_warning(validar_establecimiento(comunicacion, establecimiento),
+                 "Faltan datos del establecimiento en la fila 1.")
+})
+
+test_that("validar_establecimiento allows valid establishments with at least 7 fields", {
+  comunicacion <- data.frame(comunicacion_pk = 1:3)
+  establecimiento <- data.frame(comunicacion_fk = 1:3, codigo = c(NA, NA, NA),
+                                pais = c("ESP", "ESP", "FRA"), codigoMunicipio = c(28079, '08019', NA),
+                                nombreMunicipio = c(NA, NA, "Paris"), extra1 = 1, extra2 = 2, extra3 = 3, extra4 = 4)
+
+  expect_silent(validar_establecimiento(comunicacion, establecimiento))
+})
+
+test_that("validar_establecimiento detects missing municipality code for Spain", {
+  comunicacion <- data.frame(comunicacion_pk = 1:3)
+  establecimiento <- data.frame(comunicacion_fk = 1:3, codigo = c(NA, NA, NA),
+                                pais = c("ESP", "ESP", "ESP"), codigoMunicipio = c(18003, 18003, 18003),
+                                nombreMunicipio = c(NA, "NA", NA), extra1 = 1, extra2 = 2, extra3 = 3, extra4 = 4, extra5 = 5)
+
+  expect_warning(validar_establecimiento(comunicacion, establecimiento),
+                 "establecimiento \\(fila 2\\) -> Si el país es España \\(ESP\\), el código de municipio ha de ir informado, no así el nombre del municipio.")
+})
+
+test_that("validar_establecimiento detects incorrect foreign municipality data", {
+  comunicacion <- data.frame(comunicacion_pk = 1:3)
+  establecimiento <- data.frame(comunicacion_fk = 1:3, codigo = c(NA, NA, NA),
+                                pais = c("FRA", "USA", "DEU"), codigoMunicipio = c(NA, NA, 18003),
+                                nombreMunicipio = c("NA", "New York", NA), extra1 = 1, extra2 = 2, extra3 = 3, extra4 = 4)
+
+  expect_warning(validar_establecimiento(comunicacion, establecimiento),
+                 "establecimiento \\(fila 3\\) -> Si el país es distinto de España \\(ESP\\), ha de informar el nombre del municipio, no así el código de municipio.")
+})
+
+test_that("validar_establecimiento detects both code and other data present", {
+  comunicacion <- data.frame(comunicacion_pk = 1:3)
+  establecimiento <- data.frame(comunicacion_fk = 1:3, codigo = c(1001, NA, NA),
+                                pais = c("ESP", "ESP", "FRA"), codigoMunicipio = c(12345, 18003, NA),
+                                nombreMunicipio = c(NA, NA, "Paris"), extra1 = 1, extra2 = 2, extra3 = 3, extra4 = 4)
+
+  expect_warning(validar_establecimiento(comunicacion, establecimiento),
+                 "establecimiento \\(fila 1\\) -> Ha de indicar los datos del establecimiento o bien el código de establecimiento.")
+})
+
+test_that("validar_establecimiento passes when all conditions are met", {
+  comunicacion <- data.frame(comunicacion_pk = 1:3)
+  establecimiento <- data.frame(comunicacion_fk = 1:3, codigo = c(1001, NA, NA),
+                                pais = c(NA, "ESP", "FRA"), codigoMunicipio = c(NA, 18003, NA),
+                                nombreMunicipio = c(NA, NA, "Paris"), extra1 = c(NA,1,1), extra2 = c(NA,1,1), extra3 = c(NA,1,1), extra4 = c(NA,1,1))
+
+  expect_silent(validar_establecimiento(comunicacion, establecimiento))
 })
